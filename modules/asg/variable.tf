4d8a5b3d956150d2fd3e9a273ae3243acc5c0830
mbabidstella <130124566+mbabidstella@users.noreply.github.com>
2024-01-23 11:15:31 -0500

    Add files via upload

---
+++ modules/asg/variable.tf
@@ -0,0 +1,14 @@
+variable "project_name" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "key_name" {}
+variable "desired_capacity" {}
+variable "Prod-NAT-ALB-Sbn_az1_id" {}
+variable "bastionHost_security_group_id" {}
+variable "Prod-webserver-Sbn_az1_id" {}
+variable "Prod-webserver-Sbn_az2_id" {}
+variable "Prod-Appserver-Sbn_az1_id" {}
+variable "Prod-Appserver-Sbn_az2_id" {}
+variable "Prod-databbase-Sbn_az1_id" {}
+variable "Prod-databbase-Sbn_az2_id" {}
+variable "frontend-alb_target_group_arn" {}
